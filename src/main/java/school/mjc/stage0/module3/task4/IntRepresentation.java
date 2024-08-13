0b5f798f5774459ea4cbda44251a7459fdc34867
Avazkhon <avazxonnazirov334@gmail.com>
2024-08-13 13:58:08 +0500

    first commit

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        int[] asciiValues = {99, 104, 97, 114};
+
+        for (int ascii : asciiValues) {
+            System.out.print((char) ascii);
+        }
     }
 }
